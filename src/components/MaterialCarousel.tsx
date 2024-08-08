9d64ad1b229c6a26488cc3faa977dad806a7dcb7
sumittttpaul <sumitpaul.work@gmail.com>
2024-08-08 21:07:11 +0530

    Added new material carousel.

--- src/components/MaterialCarousel.tsx
+++ src/components/MaterialCarousel.tsx
@@ -1,53 +1,93 @@
 "use client";
 
+import { Controller, Virtual } from "swiper/modules";
+import { Swiper as SwiperTypes } from "swiper/types";
 import Image, { StaticImageData } from "next/image";
-import Script from "next/script";
-import "styles/swiper.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useState } from "react";
+import "swiper/css";
 
-// Edit "swiper-material" class in "swiper.css" file to change the height and width and to change the spaceBetween go to the "SwiperScript.js" and at the end you will see the options.
 export default function MaterialCarousel({
   images,
 }: {
   images: StaticImageData[];
 }) {
+  const [controlledSwiper, setControlledSwiper] = useState<SwiperTypes>();
+  const [MainSwiper, setMainSwiper] = useState<SwiperTypes>();
+
+  const ThumbnailClick = (index: number) => {
+    setControlledSwiper((ThumbSwiper) => {
+      if (ThumbSwiper && MainSwiper && MainSwiper.activeIndex !== index) {
+        ThumbSwiper.slideTo(index);
+        MainSwiper.slideTo(index);
+      }
+      return ThumbSwiper;
+    });
+  };
+
   return (
-    <>
-      <Script
-        type="module"
-        src="/scripts/SwiperScript.js"
-        crossOrigin="anonymous"
-        strategy="lazyOnload"
-        async
-      />
-      <div className="box-border flex h-[250px] w-full flex-col px-2.5 sm:hidden">
-        <div className="demo-slider">
-          <div className="swiper">
-            <div className="swiper-wrapper">
-              {images.map((image, index) => {
-                return (
-                  <div key={`swiper_slide_${index}`} className="swiper-slide">
-                    <div className="swiper-material-wrapper">
-                      <div className="swiper-material-content">
-                        <Image
-                          src={image}
-                          width={154}
-                          height={250}
-                          placeholder="blur"
-                          draggable={false}
-                          alt="material_carousel_slide"
-                          data-swiper-material-scale="1.25"
-                          blurDataURL={image.blurDataURL}
-                          className="demo-material-image object-cover object-top"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="relative space-y-2">
+      <Swiper
+        virtual
+        spaceBetween={5}
+        slidesPerView={1.1}
+        modules={[Controller, Virtual]}
+        controller={{ control: controlledSwiper }}
+        onSwiper={(swiper) => setMainSwiper(swiper)}
+        style={{ paddingLeft: 10, paddingRight: 10 }}
+      >
+        {images.map((image, index) => {
+          return (
+            <SwiperSlide
+              virtualIndex={index}
+              key={`swiper_slide_${index}`}
+              className="relative h-auto w-auto overflow-hidden rounded-3xl"
+            >
+              <Image
+                src={image}
+                width={360}
+                height={200}
+                draggable={false}
+                placeholder="blur"
+                alt="material_carousel_slide"
+                blurDataURL={image.blurDataURL}
+                className="max-h-[200px] object-cover object-top"
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      <Swiper
+        virtual
+        freeMode
+        spaceBetween={5}
+        slidesPerView={3.5}
+        modules={[Controller, Virtual]}
+        onSwiper={(swiper) => setControlledSwiper(swiper)}
+        style={{ paddingLeft: 10, paddingRight: 10 }}
+      >
+        {images.map((image, index) => {
+          return (
+            <SwiperSlide
+              virtualIndex={index}
+              onClick={() => ThumbnailClick(index)}
+              key={`swiper_slide_thumb_${index}`}
+              className="relative h-auto w-auto overflow-hidden rounded-2xl"
+            >
+              <Image
+                width={110}
+                height={60}
+                src={image}
+                draggable={false}
+                placeholder="blur"
+                blurDataURL={image.blurDataURL}
+                alt="material_carousel_slide_thumb"
+                className="max-h-[60px] object-cover object-top"
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
   );
 }
